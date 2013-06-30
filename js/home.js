9bc0f2a9ddabf4dd81283f930ba155de42ff2de2
Emma Cunningham <the.cunning.ham@gmail.com>
2013-06-30 01:11:17 -0700

    mad underscore templatin

--- js/home.js
+++ js/home.js
@@ -10,6 +10,26 @@ function PageController() {
 PageController.prototype.init = function() {
 	this.randomPredicateDisplay();
 
+	this.addProjects();
+};
+
+PageController.prototype.addProjects = function() {
+	this.setupProjectTemplate();
+};
+
+PageController.prototype.setupProjectTemplate = function() {
+	var template = '<% _.each(project, function(project) { %><a class=\"project\" href=\"<%= project.href %>\">' +
+		'<div class=\"project-image\" style=\"background-image: url(<%= project.image %>);\"></div>' +
+		'<div class=\"project-description\">' +
+		'<h4><%= project.name %></h4>' +
+		'<div><%= project.description %></div></div></a> <% }); %>';
+	var data = _.template(template, {
+		project : [
+			{name: 'hello', description: 'world', href: '//www.google.com/edu', image: 'img/edu.png'},
+			{name: 'hi', description: 'universe'}]
+	});
+
+	console.log(data);
 };
 
 PageController.prototype.randomPredicateDisplay = function() {
@@ -18,8 +38,8 @@ PageController.prototype.randomPredicateDisplay = function() {
 	var temp = -1;
 
 	var getRan = function(){
-		// get the random number
-	   	var predicates = ["tautological","unbound","metacircular","echo","vague","istigkeit","now","here","reification","abstraction","spooky","curator","dreamer","hacker","dilettante","finite","phantasmagoria"];
+		// get the predicate at random
+	  var predicates = ["tautological","unbound","metacircular","echo","vague","istigkeit","now","here","reification","abstraction","spooky","curator","dreamer","hacker","dilettante","finite","phantasmagoria"];
 		var randomPredicate = predicates[Math.floor(Math.random() * predicates.length)];
 		return randomPredicate;
 	};
